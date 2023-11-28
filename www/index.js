13d98248ae4e46e4cd01b35c5ff76bf4d2f3c11a
Eric Falkenberg <eric.falkenberg@dfstudio.com>
2023-11-27 21:57:48 -0500

    add www

---
+++ www/index.js
@@ -0,0 +1,10 @@
+import * as game from "rust-wasm";
+game.run();
+
+// function gameLoop() {
+//   rust.paint_screen(universe);
+//   universe = rust.tick(universe);
+//   setTimeout(gameLoop, 1000);
+// }
+//
+// gameLoop();
